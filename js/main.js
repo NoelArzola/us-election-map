5eac9811a944c17ba59033cd5ac068712e1d2b1b
Noel Arzola Jr <noel.arzola@gmail.com>
2021-05-10 06:26:16 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,129 +1,227 @@
 /****Functions****/
 //For making a candidate
-var makeCandidate = function (name, partyColor) {
+const makeCandidate = function (name, partyColor) {
+  const candidate = {};
+  candidate.name = name;
+  candidate.electionResults = null;
+  candidate.totalVotes = 0;
+  candidate.partyColor = partyColor;
 
-    var candidate = {};
-    candidate.name = name;
-    candidate.electionResults = null;
-    candidate.totalVotes = 0;
-    candidate.partyColor = partyColor;
+  candidate.announce = function () {
+    //console.log(candidate.name + ", " + candidate.electionResults + ", " + candidate.totalVotes);
+  };
 
-    candidate.announce = function() {
-        //console.log(candidate.name + ", " + candidate.electionResults + ", " + candidate.totalVotes);
-    };
+  //method for tallying votes
+  candidate.electionResultsTotal = function () {
+    this.totalVotes = 0;
 
-    //method for tallying votes
-    candidate.electionResultsTotal = function(){
-        
-        this.totalVotes = 0;
+    for (let i = 0; i < this.electionResults.length; i++) {
+      this.totalVotes = this.totalVotes + this.electionResults[i];
+      //console.log(this.electionResultsTotal);
+    }
+  };
 
-        for (var i = 0; i < this.electionResults.length; i++) {
-            this.totalVotes = this.totalVotes + this.electionResults[i];
-            //console.log(this.electionResultsTotal);
-        }
-    };
-
-    candidate.announce();
-    return candidate;
+  candidate.announce();
+  return candidate;
 };
 
 //determines the winner in each state
-var setStateResults = function(state) {
+const setStateResults = function (state) {
+  theStates[state].winner = null;
 
-    theStates[state].winner = null;
+  if (angela.electionResults[state] > pam.electionResults[state]) {
+    theStates[state].winner = angela;
+  } else if (pam.electionResults[state] > angela.electionResults[state]) {
+    theStates[state].winner = pam;
+  }
 
-    if (johnny.electionResults[state] > martha.electionResults[state]) {
-        theStates[state].winner = johnny;
-    } else if (martha.electionResults[state] > johnny.electionResults[state]) {
-        theStates[state].winner = martha;
-    }
+  const stateWinner = theStates[state].winner;
 
-    var stateWinner = theStates[state].winner;
+  if (theStates[state].winner !== null) {
+    theStates[state].rgbColor = stateWinner.partyColor;
+  } else {
+    theStates[state].rgbColor = [11, 32, 57];
+  }
 
-    if (theStates[state].winner !== null) {
-        theStates[state].rgbColor = stateWinner.partyColor;
-    } else {
-        theStates[state].rgbColor = [11, 32, 57];
-    }
+  const statesTable = document.getElementById("stateResults"); //grabs state table
+  const header = statesTable.children[0].children[0];
+  const stateName = header.children[0];
+  stateName.innerText = theStates[state].nameFull;
+  const stateAbbrev = header.children[1];
+  stateAbbrev.innerText = theStates[state].nameAbbrev;
 
-    var statesTable = document.getElementById("stateResults"); //grabs state table
-    var header = statesTable.children[0].children[0];
-    var stateName = header.children[0];
-    stateName.innerText = theStates[state].nameFull;
-    var stateAbbrev = header.children[1];
-    stateAbbrev.innerText = theStates[state].nameAbbrev;
+  //the other three rows
+  const statesRow = statesTable.children[1];
+  const name1 = statesRow.children[0].children[0];
+  name1.innerText = angela.name;
+  const results1 = statesRow.children[0].children[1];
+  results1.innerText = angela.electionResults[state];
 
-    //the other three rows
-    var statesRow = statesTable.children[1];
-    var name1 = statesRow.children[0].children[0];
-    name1.innerText = johnny.name;
-    var results1 = statesRow.children[0].children[1];
-    results1.innerText = johnny.electionResults[state];
+  const name2 = statesRow.children[1].children[0];
+  name2.innerText = pam.name;
+  const results2 = statesRow.children[1].children[1];
+  results2.innerText = pam.electionResults[state];
 
-    var name2 = statesRow.children[1].children[0];
-    name2.innerText = martha.name;
-    var results2 = statesRow.children[1].children[1];
-    results2.innerText = martha.electionResults[state];
+  const winnerName = statesRow.children[2].children[1];
 
-    var winnerName = statesRow.children[2].children[1];
-
-    if (stateWinner !== null) {
-        winnerName.innerText = stateWinner.name;
-    } else {
-        winnerName.innerText ="DRAW";
-    }
-    
-}
+  if (stateWinner !== null) {
+    winnerName.innerText = stateWinner.name;
+  } else {
+    winnerName.innerText = "DRAW";
+  }
+};
 /***End Functions***/
 
 /****Variables****/
 //candidate variables with names and colors
-var johnny = makeCandidate ("Johnny Stone", [132, 17, 11]);
-var martha = makeCandidate ("Martha Douglas", [245, 141, 136]);
-var winner = null; //for determining the winner with if/else
+const angela = makeCandidate("Angela Martin", [132, 17, 11]);
+const pam = makeCandidate("Pam Beesley", [245, 141, 136]);
+let winner = null; //for determining the winner with if/else
 /***End Variables***/
 
 //voting data
-johnny.electionResults = [5,1,7,2,33,6,4,2,1,14,8,3,1,11,11,0,5,3,3,3,7,4,8,9,3,7,2,2,4,2,8,3,15,15,2,12,0,4,13,1,3,2,8,21,3,2,11,1,3,7,2];
-martha.electionResults = [4,2,4,4,22,3,3,1,2,15,8,1,3,9,0,6,1,5,5,1,3,7,8,1,3,3,1,3,2,2,6,2,14,0,1,6,7,3,7,3,6,1,3,17,3,1,2,11,2,3,1];
+angela.electionResults = [
+  5,
+  1,
+  7,
+  2,
+  33,
+  6,
+  4,
+  2,
+  1,
+  14,
+  8,
+  3,
+  1,
+  11,
+  11,
+  0,
+  5,
+  3,
+  3,
+  3,
+  7,
+  4,
+  8,
+  9,
+  3,
+  7,
+  2,
+  2,
+  4,
+  2,
+  8,
+  3,
+  15,
+  15,
+  2,
+  12,
+  0,
+  4,
+  13,
+  1,
+  3,
+  2,
+  8,
+  21,
+  3,
+  2,
+  11,
+  1,
+  3,
+  7,
+  2,
+];
+pam.electionResults = [
+  4,
+  2,
+  4,
+  4,
+  22,
+  3,
+  3,
+  1,
+  2,
+  15,
+  8,
+  1,
+  3,
+  9,
+  0,
+  6,
+  1,
+  5,
+  5,
+  1,
+  3,
+  7,
+  8,
+  1,
+  3,
+  3,
+  1,
+  3,
+  2,
+  2,
+  6,
+  2,
+  14,
+  0,
+  1,
+  6,
+  7,
+  3,
+  7,
+  3,
+  6,
+  1,
+  3,
+  17,
+  3,
+  1,
+  2,
+  11,
+  2,
+  3,
+  1,
+];
 
-//console.log(johnny.electionResults, martha.electionResults);
+//console.log(angela.electionResults, pam.electionResults);
 
 //re-counts! updates numbers
-johnny.electionResults[9] = 1;
-martha.electionResults[9] = 28;
+angela.electionResults[9] = 1;
+pam.electionResults[9] = 28;
 
-johnny.electionResults[4] = 17;
-martha.electionResults[4] = 38;
+angela.electionResults[4] = 17;
+pam.electionResults[4] = 38;
 
-johnny.electionResults[43] = 11;
-martha.electionResults[43] = 27;
+angela.electionResults[43] = 11;
+pam.electionResults[43] = 27;
 
-//console.log(johnny.electionResults, martha.electionResults, johnny.partyColor, martha.partyColor);
+//console.log(angela.electionResults, pam.electionResults, angela.partyColor, pam.partyColor);
 
 //calling the results methods
-johnny.electionResultsTotal();
-martha.electionResultsTotal();
+angela.electionResultsTotal();
+pam.electionResultsTotal();
 
 //determining the winner
-if (johnny.totalVotes < martha.totalVotes) {
-    winner = martha.name;
-} 
-else if (martha.totalVotes < johnny.totalVotes)
-{
-    winner = johnny.name;
+if (angela.totalVotes < pam.totalVotes) {
+  winner = pam.name;
+} else if (pam.totalVotes < angela.totalVotes) {
+  winner = angela.name;
 } else {
-winner = "DRAW";
+  winner = "DRAW";
 }
 
 //console.log(winner);
 
-var countryTable = document.getElementById("countryResults"); //grabs country table
-var row = countryTable.children[0].children[0];
+const countryTable = document.getElementById("countryResults"); //grabs country table
+const row = countryTable.children[0].children[0];
 
 //populates the country table
-row.children[0].innerText = johnny.name;
-row.children[1].innerText = johnny.totalVotes;
-row.children[2].innerText = martha.name;
-row.children[3].innerText = martha.totalVotes;
+row.children[0].innerText = angela.name;
+row.children[1].innerText = angela.totalVotes;
+row.children[2].innerText = pam.name;
+row.children[3].innerText = pam.totalVotes;
 row.children[5].innerText = winner;
